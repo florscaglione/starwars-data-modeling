1d4990c4106a4f7f46be810f6816cc780020c5ca
florscaglione <florencia.scaglione@hotmail.com>
2021-09-11 01:30:02 +0000

    diagrama starwars

--- src/models.py
+++ src/models.py
@@ -8,23 +8,66 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(20), nullable=False)
+    #people = relationship("people")
+    
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    height = Column(String(50))
+    mass = Column(String(50))
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+    birth_year = Column(String(50))
+    gender = Column(String(50))
+    name = Column(String(50), nullable=False)
+    img_url: Column(String)
+    homeworld = relationship("Planet", back_populates="people")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="favorites_person")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter = Column(String(50))
+    rotation_period = Column(String(50))
+    orbital_period = Column(String(50))
+    gravity = Column(String(50))
+    population = Column(String(50))
+    climate = Column(String(50))
+    terrain = Column(String(50))
+    surface_water = Column(String(50))
+    name = Column(String(50), nullable=False)
+    img_url: Column(String)
+    people = relationship("People")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="favorites_planets")
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(50))
+    starship_class = Column(String(50))
+    manufacturer = Column(String(50))
+    cost_in_credits = Column(String(50))
+    length = Column(String(50))
+    crew = Column(String(50))
+    passengers = Column(String(50))
+    max_atmosphering_speed = Column(String(50))
+    hyperdrive_rating = Column(String(50))
+    MGLT = Column(String(50))
+    cargo_capacity = Column(String(50))
+    consumables = Column(String(50))
+    name = Column(String(50), nullable=False)
+    img_url: Column(String)
+    #people = relationship("People", uselist=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User")
 
     def to_dict(self):
         return {}
